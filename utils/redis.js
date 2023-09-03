d8cbcb03052363e548030861caf8a21e3609f18f
vickouma77 <Vickprestone20@gmail.com>
2023-09-03 16:04:30 +0300

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -13,6 +13,23 @@ class RedisClient {
   isAlive() {
     return this.client.connected;
   }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) reject(err);
+        resolve(value);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
 }
 
 const redisClient = new RedisClient();
