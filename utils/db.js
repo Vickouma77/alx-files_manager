cba4ef31c3b7e0e8c1539f68a61e1c5b8c6c8d59
vickouma77 <Vickprestone20@gmail.com>
2023-09-03 16:54:57 +0300

    MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -3,27 +3,27 @@
 const { MongoClient } = require('mongodb');
 
 class DBClient {
-    constructor() {
-        this.db = null;
-        MongoClient.connect(process.env.DB_HOST || 'mongodb://localhost:27017', { useUnifiedTopology: true }, (err, client) => {
-        if (err) console.log(err);
-        else {
-            this.db = client.db(process.env.DB_DATABASE || 'files_manager');
-        }
-        });
-    }
-    
-    isAlive() {
-        return !!this.db;
-    }
-    
-    async nbUsers() {
-        return this.db.collection('users').countDocuments();
-    }
-    
-    async nbFiles() {
-        return this.db.collection('files').countDocuments();
-    }
+  constructor() {
+    this.db = null;
+    MongoClient.connect(process.env.DB_HOST || 'mongodb://localhost:27017', { useUnifiedTopology: true }, (err, client) => {
+      if (err) console.log(err);
+      else {
+        this.db = client.db(process.env.DB_DATABASE || 'files_manager');
+      }
+    });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
