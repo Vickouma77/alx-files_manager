aa7a5bea3bad01faa6e7bbaeaefe02ff1590f5c9
vickouma77 <Vickprestone20@gmail.com>
2023-09-04 15:06:21 +0300

    First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,23 +5,15 @@ const { redisClient } = require('../utils/redis');
 
 
 class AppController {
-  static async getStatus(req, res) {
-    const redisStatus = redisClient.isAlive();
-    const dbStatus = dbClient.isAlive();
-
-    if (redisStatus && dbStatus) {
-      return res.status(200).json({ redis: true, db: true });
-    } else {
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-  }
+  static getStatus(request, response) {
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  };
 
   static async getStats(req, res) {
-    const usersCount = await dbClient.nbUsers();
-    const filesCount = await dbClient.nbFiles();
-
-    return res.status(200).json({ users: usersCount, files: filesCount });
-  }
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    response.status(200).json({ users, files });
+  };
 }
 
 module.exports = AppController;
