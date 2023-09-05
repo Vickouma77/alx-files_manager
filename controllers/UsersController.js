cc202f01879c3149b8a27a5bd5d5be502c05a77f
vickouma77 <Vickprestone20@gmail.com>
2023-09-05 15:44:32 +0300

    configuring server to parse JSON data

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,18 +1,43 @@
 #!/usr/bin/node
 
-import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import sha1 from 'sha1';
 
 class UsersController {
-  static async postNew(request, response) {
-    const { email, password } = request.body;
-    if (!email) return response.status(400).json({ error: 'Missing email' });
-    if (!password) return response.status(400).json({ error: 'Missing password' });
-    const userExists = await dbClient.users.findOne({ email });
-    if (userExists) return response.status(400).json({ error: 'Already exist' });
-    const user = await dbClient.users.insertOne({ email, password: sha1(password) });
-    return response.status(201).json({ id: user.insertedId, email });
-  }
+    static async postNew(request, response) {
+        const { email, password } = request.body;
+
+        // Check if email and password are provided
+        if (!email) return response.status(400).json({ error: 'Missing email' });
+        if (!password) return response.status(400).json({ error: 'Missing password' });
+
+        try {
+            // Check if a user with the same email already exists in the database
+            const userExists = await dbClient.users.findOne({ email });
+
+            if (userExists) {
+                return response.status(400).json({ error: 'User with this email already exists' });
+            }
+
+            // Hash the password
+            const hashedPassword = sha1(password);
+
+            // Create a new user
+            const newUser = {
+                email,
+                password: hashedPassword,
+            };
+
+            // Insert the new user into the database
+            const user = await dbClient.users.insertOne(newUser);
+
+            // Respond with the user's ID and email
+            return response.status(201).json({ id: user.insertedId, email });
+        } catch (error) {
+            console.error(error);
+            return response.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
 }
 
 module.exports = UsersController;
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const router = require('./routes/index');
 
 const port = parseInt(process.env.PORT, 10) || 5000;
 
-app.use('/', router);
 app.use(express.json());
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
