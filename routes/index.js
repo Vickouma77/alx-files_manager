ba29c80f06a566acc0d422ebe8f679fae41bf2ea
vickouma77 <Vickprestone20@gmail.com>
2023-09-05 23:19:18 +0300

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
 #!/usr/bin/node
 
-const express = require('express');
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/status', AppController.getStatus);
+
 router.get('/stats', AppController.getStats);
+
 router.post('/users', UsersController.postNew);
+
 router.get('/connect', AuthController.getConnect);
+
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/user/me', UsersController.getMe);
+
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
