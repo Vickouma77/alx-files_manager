0ad9b28d1f2e6c5733eb5c9d7d5309cb5eb59a4b
vickouma77 <Vickprestone20@gmail.com>
2023-09-05 16:28:32 +0300

    configuring server to parse JSON data

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,32 +6,21 @@ import dbClient from '../utils/db';
 class UsersController {
   static async postNew(request, response) {
     const { email, password } = request.body;
-
-    // Check if email and password are provided
     if (!email) return response.status(400).json({ error: 'Missing email' });
     if (!password) return response.status(400).json({ error: 'Missing password' });
 
     try {
-      // Check if a user with the same email already exists in the database
       const userExists = await dbClient.users.findOne({ email });
 
       if (userExists) {
         return response.status(400).json({ error: 'User with this email already exists' });
       }
-
-      // Hash the password
       const hashedPassword = sha1(password);
-
-      // Create a new user
       const newUser = {
         email,
         password: hashedPassword,
       };
-
-      // Insert the new user into the database
       const user = await dbClient.users.insertOne(newUser);
-
-      // Respond with the user's ID and email
       return response.status(201).json({ id: user.insertedId, email });
     } catch (error) {
       console.error(error);
