50f2a70715e1640b4f616bce224615c13be25ac8
vickouma77 <Vickprestone20@gmail.com>
2023-09-05 14:58:11 +0300

    creating new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
-import dbClient from '../utils/db';
 import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
 class UsersController {
-    static async postNew(request, response) {
-        const { email, password } = request.body;
-        if (!email) return response.status(400).json({ error: 'Missing email' });
-        if (!password) return response.status(400).json({ error: 'Missing password' });
-        const userExists = await dbClient.users.findOne({ email });
-        if (userExists) return response.status(400).json({ error: 'Already exist' });
-        const user = await dbClient.users.insertOne({ email, password: sha1(password) });
-        return response.status(201).json({ id: user.insertedId, email });
-    }
+  static async postNew(request, response) {
+    const { email, password } = request.body;
+    if (!email) return response.status(400).json({ error: 'Missing email' });
+    if (!password) return response.status(400).json({ error: 'Missing password' });
+    const userExists = await dbClient.users.findOne({ email });
+    if (userExists) return response.status(400).json({ error: 'Already exist' });
+    const user = await dbClient.users.insertOne({ email, password: sha1(password) });
+    return response.status(201).json({ id: user.insertedId, email });
+  }
 }
 
 module.exports = UsersController;
