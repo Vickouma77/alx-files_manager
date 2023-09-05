207fc6d93986f428883fa83dff6ee0f3b5833bf9
vickouma77 <Vickprestone20@gmail.com>
2023-09-05 15:04:22 +0300

    configuring server to parse JSON data

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const router = require('./routes/index');
 const port = parseInt(process.env.PORT, 10) || 5000;
 
 app.use('/', router);
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
