f5a7ed781e6afb458aa74ea3a78ffc2e42811557
vickouma77 <Vickprestone20@gmail.com>
2023-09-06 13:11:18 +0300

    First file endpoints

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -18,5 +19,7 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 
 router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
