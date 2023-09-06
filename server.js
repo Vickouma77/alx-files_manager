d642729c39fa06bf9b34f2ac88905e48e93776e5
vickouma77 <Vickprestone20@gmail.com>
2023-09-06 15:13:46 +0300

    update

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
-const express = require('express');
-
-const app = express();
-const router = require('./routes/index');
+import express from 'express';
+import router from './routes/index';
 
 const port = parseInt(process.env.PORT, 10) || 5000;
 
-app.use(express.json());
+const app = express();
 
+app.use(express.json());
 app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
 
-module.exports = app;
+export default app;
