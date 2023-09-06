79cc07057149603b7f4c791f1874164dc12a0b9d
vickouma77 <Vickprestone20@gmail.com>
2023-09-06 13:27:43 +0300

    Get and list file

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,8 @@ router.get('/users/me', UsersController.getMe);
 
 router.post('/files', FilesController.postUpload);
 
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
 module.exports = router;
